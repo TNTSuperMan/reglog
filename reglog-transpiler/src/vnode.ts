a777d6e5407429fe6bc1ba5168cb5867690ef5bb
TNTSuperMan <tntsuperga@gmail.com>
2025-01-06 15:19:59 +0900

    Elementに修正

--- reglog-transpiler/src/vnode.ts
+++ reglog-transpiler/src/vnode.ts
@@ -5,7 +5,7 @@ export type VText = string;
 export const VNodeFromDOM = (dom: ChildNode): VNode => {
     if(dom instanceof window.Text){
         return dom.textContent ?? "";
-    }else if(dom instanceof window.HTMLElement){
+    }else if(dom instanceof window.Element){
         return [dom.tagName,
             Array.from(dom.attributes).map(e=>[e.name, e.value]),
             ...Array.from(dom.childNodes).map(VNodeFromDOM).filter(e=>e!="")]
