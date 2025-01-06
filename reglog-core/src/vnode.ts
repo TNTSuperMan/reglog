290cf42e36daae2e199ed00822771ad03fc24bd1
TNTSuperMan <tntsuperga@gmail.com>
2025-01-06 15:53:23 +0900

    Windowを消す

--- reglog-core/src/vnode.ts
+++ reglog-core/src/vnode.ts
@@ -2,13 +2,13 @@ export type VNode = [string, [string,string][], ...VNode[]] | string;
 
 export const toDOM = (content: VNode[]): Node[] => {
     const toHTMLDom = (tag: string, attributes: [string,string][], ...childs: VNode[]): HTMLElement => {
-        const el = window.document.createElement(tag);
+        const el = document.createElement(tag);
         attributes.forEach(e=>el.setAttribute(...e));
         el.append(...childs.map(e=>toTextOrHTML(e)))
         return el;
     }
     const toTextOrHTML = (content: VNode) =>typeof content == "string" ?
-        new window.Text(content) :
+        new Text(content) :
         toHTMLDom(...content)
     return content.map(toTextOrHTML);
 }
