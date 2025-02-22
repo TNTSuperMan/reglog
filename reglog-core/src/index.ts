8128b5ace418be73c7b0d4fc88fed00a5448dfb1
TNTSuperMan <tntsuperga@gmail.com>
2025-02-22 18:43:41 +0900

    ?を消す

--- reglog-core/src/index.ts
+++ reglog-core/src/index.ts
@@ -9,13 +9,13 @@ export default (root: HTMLElement, routes: {[key: string]: (()=>VNode[] | Promis
             res(0))
     .then(e=>{
         const update = () => {
-            while(root?.firstChild)
-                root?.firstChild.remove();
+            while(root.firstChild)
+                root.firstChild.remove();
             Promise.resolve().then(e=>(
                 routes[w.location.hash||""] ??
                 routes["404"] ??
                 (()=>["Not Found!"]))())
-            .then(e=>root?.append(...toDOM(e)))
+            .then(e=>root.append(...toDOM(e)))
         }
         window.addEventListener("hashchange", update);
     })
